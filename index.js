c6af180fafbfef266659f9f2dc14248208ab3a82
Aleen <pandapubgnp1@gmail.com>
2022-11-28 15:30:51 +0545

    Styled Shortcuts

--- index.js
+++ index.js
@@ -42,15 +42,17 @@ class Shortcut {
         div.className = "Haribijok"
         div.style = `
         display: inline-block !important;
-        border: 1px solid #9C3D54 !important;
+        border: none !important;
+        outline:none !important;
         border-radius: 4px !important;
         padding: 0.1em 0.2em !important;
         margin: 0 0.2em !important;
-        box-shadow: 0 1px 0px rgba(0, 0, 0, 0.2), 0 0 0 2px #E2703A  !important;
-        background-color: #310B0B !important;
+        box-shadow: 0 1px 0px rgba(0, 0, 0, 0.2), 0 0 0 2px #434242  !important;
+        background-color: #222222 !important;
         font-family:inherit !important;
-        font-size:0.8em !important;
-        color: #EEB76B !important;
+        font-size:16px !important;
+        color: #F3EFE0 !important;
+        font-weight:bold !important;
     
 
         
@@ -63,17 +65,21 @@ class Shortcut {
         document.addEventListener("keydown", (e) => {
 
 
-            this.keyCode = e.key.toLocaleLowerCase();
+            try {
+                this.keyCode = e.key.toLocaleLowerCase();
 
-            if (e.ctrlKey && e.altKey && this.keyCode) {
-                e.preventDefault();
+                if (e.ctrlKey && e.altKey && this.keyCode) {
+                    e.preventDefault();
 
-                this.getIndex = this.generateList.indexOf(this.keyCode.toUpperCase());
+                    this.getIndex = this.generateList.indexOf(this.keyCode.toUpperCase());
 
-                console.log(this.getIndex, this.keyCode);
+                    console.log(this.getIndex, this.keyCode);
 
-                document.querySelectorAll(`.${this.link}`)[this.getIndex].click();
+                    document.querySelectorAll(`.${this.link}`)[this.getIndex].parentElement.click();
 
+                }
+            } catch (error) {
+                console.log(error);
             }
 
         })
@@ -92,3 +98,29 @@ googleHeading.query();
 googleHeading.generateShortcuts();
 
 
+// have finished google shortcut keys
+
+// lets create a class which blackllists sites
+const options = {
+    method: 'GET',
+    headers: {
+        'Content-type': 'application/json',
+        'Key': '26db254d00c48cc2ddfd049278b161f31c9ebfb135379553685243e55c9b67a016ec269c7f1acf80'
+    },
+ 
+};
+
+const querystring= {
+    'ipAddress': '118.25.6.39',
+    'maxAgeInDays': '90'
+}
+fetch("https://api.abuseipdb.com/api/v2/check/",{
+    method:'GET',
+    headers:{
+        key: '26db254d00c48cc2ddfd049278b161f31c9ebfb135379553685243e55c9b67a016ec269c7f1acf80',
+        Accept: 'application/json'
+      
+    },
+ 
+    Cache: 'default'
+})
